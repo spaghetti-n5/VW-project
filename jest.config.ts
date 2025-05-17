e6eeac42857365f370953c522f06b62f3e10cb10
Eleonora Baret <eleonora.baret@gmail.com>
2025-05-17 11:13:56 +0200

    feat: jest-react-testing-library setup

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(svg|png|jpg|jpeg|gif)$": "<rootDir>/__mocks__/fileMock.ts",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+};
+
+export default config;
