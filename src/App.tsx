9bc27f19c282cf88ce546b5272f98029fd8fc348
Eleonora Baret <eleonora.baret@gmail.com>
2025-05-20 12:12:20 +0200

    feat: lazy load routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import TablePage from './pages/TablePage';
 import { Routes, Route } from 'react-router-dom';
-import Header from './components/shared/Header';
-import FavoritesPage from './pages/FavoritesPage';
+import { lazy, Suspense } from 'react';
+import LoadingSpinner from './components/shared/LoadingSpinner';
+
+// Lazy load components
+const TablePage = lazy(() => import('./pages/TablePage'));
+const FavoritesPage = lazy(() => import('./pages/FavoritesPage'));
+const Header = lazy(() => import('./components/shared/Header'));
 
 const App = () => (
-  <>
+  <Suspense fallback={<LoadingSpinner />}>
     <Header />
     <Routes>
       <Route path="VW-project" element={<TablePage />} />
       <Route path="VW-project/favorites" element={<FavoritesPage />} />
     </Routes>
-  </>
+  </Suspense>
 );
 
 export default App;
